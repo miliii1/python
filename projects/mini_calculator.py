a14cc8457152bc3b26f454c823581bb694d191f3
milagros gonzalez <milagrosgzlez@gmail.com>
2025-02-25 11:21:23 -0300

    Listo

--- projects/mini_calculator.py
+++ projects/mini_calculator.py
@@ -1,3 +1,16 @@
-x = input("Ingrese el primer número: ")
-y = input("Ingrese el segundo número: ")
-print(x, y)
+x = int(input("Ingrese el primer número: "))
+y = int(input("Ingrese el segundo número: "))
+print(f"Tus número ingresados son: {x} y {y}")
+
+operator = input("Ingrese la operación: ")
+
+if operator == '+':
+    print(f"La suma de {x} y {y} es:", x + y)
+elif operator == '-':
+    print(f"La resta de {x} y {y} es", x - y)
+elif operator == '*':
+    print(f"La múltiplicacion de {x} y {y} es:", x * y)
+elif operator == '/':
+    print(f"La división de {x} y {y} es:", x / y)
+else:
+    print("Operación no disponible")
